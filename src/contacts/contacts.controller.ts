6c1666e8131dc3a363fd7e554b3edc330641b477
Andres Duque <analista.desarrollo3@hyundailatinoamerica.com>
2023-02-11 19:56:53 -0500

    feat: service and controller succesfully

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -3,6 +3,7 @@ import {
   Get,
   Post,
   Body,
+  Res,
   Patch,
   Param,
   Delete,
@@ -14,16 +15,16 @@ import { ContactsService } from './contacts.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
 import { Contact } from './entities/contact.entity';
-
+import { Response } from 'express';
 @Controller('contacts')
 export class ContactsController {
   constructor(private readonly contactsService: ContactsService) {}
 
   @Post()
-  async createContact(@Body() contact: CreateContactDto) {
+  async createContact(@Body() contact: CreateContactDto, @Res() res: Response) {
     if (!contact.cellphone || !contact.name || !contact.last_name)
       return new HttpException(
-        ' Celular, nombre y apellido son requeridos',
+        'Celular, nombre y apellido son requeridos',
         HttpStatus.BAD_REQUEST,
       );
 
@@ -57,6 +58,11 @@ export class ContactsController {
     @Param('id', ParseIntPipe) id: number,
     @Body() updateContactDto: UpdateContactDto,
   ) {
+    if (!updateContactDto)
+      return new HttpException(
+        'No se recibieron los parametros para actualizar',
+        HttpStatus.BAD_REQUEST,
+      );
     if (updateContactDto?.cellphone) {
       const contactFound = await this.contactsService.findContactByPhone(
         updateContactDto.cellphone,
@@ -67,11 +73,21 @@ export class ContactsController {
           HttpStatus.CONFLICT,
         );
     }
-    return this.contactsService.updateContact(id, updateContactDto);
+
+    const contactUpdated = await this.contactsService.updateContact(
+      id,
+      updateContactDto,
+    );
+    if (!contactUpdated.affected)
+      return new HttpException('Contacto no encontrado', HttpStatus.NOT_FOUND);
+    return contactUpdated;
   }
 
   @Delete(':id')
-  deleteContact(@Param('id', ParseIntPipe) id: number) {
-    return this.contactsService.deleteContact(id);
+  async deleteContact(@Param('id', ParseIntPipe) id: number) {
+    const userDelete = await this.contactsService.deleteContact(id);
+    if (!userDelete.affected)
+      return new HttpException('Contacto no encontrado', HttpStatus.NOT_FOUND);
+    return userDelete;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ClassSerializerInterceptor } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  //port app
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   const port = process.env.PORT_APP ?? 3000;
   await app.listen(port);
 }
